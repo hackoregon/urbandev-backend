a2263fcbb6e673686ead0ce2df587cd3e134f0a3
Jamil Alvi <jamil@quarkspring.com>
2015-10-28 08:51:27 -0700

    Removed duplicate doc directory

--- other/doc/docapp.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-   /* Dynamically add a <style> tag to the head */
-   var ss = document.createElement("link");
-   ss.type = "text/css";
-   ss.rel = "stylesheet";
-   ss.href = "/css/more_doc_styles.css";
-   document.getElementsByTagName("head")[0].appendChild(ss);
-});
